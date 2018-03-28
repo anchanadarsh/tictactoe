285b63ad8ab840c92e82d9e72a1a4e6bbefe1706
Adarsh Anchan <anchanadarsh@gmail.com>
2018-03-28 19:02:39 +0530

    asf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
-var a = angular.module("myApp", []);
-a.controller("myController", ['$scope', function ($scope) {
-    
+var xo = angular.module("tictactoe", []);
+xo.controller("xoController", ['$scope', function ($scope) {
+
+    $scope.tttTiles = ['', '', '', '', '', '', '', '', ''];
+
+    $scope.tileClick = function () {
+        $scope.getTileIndex = jQuery(this);
+        console.log($scope.getTileIndex);
+    }
+
 }]);
